5ff584229435e915d2ebe2ee811e0f7f713ee4f8
commando10M <82320738+commando10M@users.noreply.github.com>
2024-12-03 19:15:33 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from tradingview_ta import *
+import time
+
+totalgain = 0
 
 bitcoin = TA_Handler(
     symbol="BTCUSD",
@@ -7,7 +10,47 @@ bitcoin = TA_Handler(
     interval="1m",
     timeout=None
 )
-totalgain = 0
+
+def main():
+    global time
+    while True:
+        trade = False
+        price = bitcoin.get_indicators()['close']
+        rsi = bitcoin.get_indicators()['RSI']
+        sma20 = bitcoin.get_indicators()['SMA20']
+        sma50 = bitcoin.get_indicators()['SMA50']
+        sma200 = bitcoin.get_indicators()['SMA200']
+        if trade == False :
+            if sma20 < sma200:
+                time.sleep(150)
+                exp = time.time()
+                while sma200 > price:
+                    price = bitcoin.get_indicators()['close']
+                    sma20 = bitcoin.get_indicators()['SMA20']
+                    sma50 = bitcoin.get_indicators()['SMA50']
+                    sma200 = bitcoin.get_indicators()['SMA200']
+                    #if exp - time.time() > 1000:
+                        #main()
+                trade = True
+                price = bitcoin.get_indicators()['close']
+                rsi = bitcoin.get_indicators()['RSI']
+                priceTB = buy(price)
+                while trade:
+                    time.sleep(100)
+                    price = bitcoin.get_indicators()['close']
+                    rsi = bitcoin.get_indicators()['RSI']
+                    sma20 = bitcoin.get_indicators()['SMA20']
+                    sma50 = bitcoin.get_indicators()['SMA50']
+                    if sma50 < sma20:
+                        time.sleep(100)
+                        while trade:
+                            sma20 = bitcoin.get_indicators()['SMA20']
+                            sma50 = bitcoin.get_indicators()['SMA50']
+                            if sma50 > sma20:
+                                sell(price, priceTB)
+                                trade = False
+
+
 def buy(priceTB):
     global price
     print("buy at " + str(priceTB))
@@ -15,27 +58,12 @@ def buy(priceTB):
 
 def sell(priceTS, priceTB1):
     global totalgain
-    gain = (priceTS - priceTB1) / priceTB
+    priceTS = bitcoin.get_indicators()['close']
+    gain = (priceTS - priceTB1) / priceTB1
     totalgain = totalgain + gain
-    print("sell at " + str(priceTS) + " with " + str(gain) + "% gain this is "+ totalgain +"% total gain.")
+    print("sell at " + str(priceTS) + " with " + str(gain) + "% gain \n"+ str(totalgain) +"% total gain.")
+    time.sleep(150)
 
 
 while True:
-    trade = False
-    price = bitcoin.get_indicators()['close']
-    rsi = bitcoin.get_indicators()['RSI']
-    sma = bitcoin.get_indicators()['SMA20']
-    print(sma < price)
-    if trade == False :
-        if rsi < 41 and sma < price:
-            trade = True
-            price = bitcoin.get_indicators()['close']
-            rsi = bitcoin.get_indicators()['RSI']
-            priceTB = buy(price)
-            while trade:
-                price = bitcoin.get_indicators()['close']
-                rsi = bitcoin.get_indicators()['RSI']
-                sma = bitcoin.get_indicators()['SMA20']
-                if rsi > 59 and sma > price:
-                    sell(price, priceTB)
-                    trade = False
+    main()
